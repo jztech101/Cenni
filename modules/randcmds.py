59162779c6ad5b5b2bd5c6adb1dcaa3df6f85b23
Julian Zhou <jzhou2oo6@gmail.com>
2018-06-15 06:25:08 +0000

    Working Topic Command :D

---
+++ modules/randcmds.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3# -*- coding: utf-8 -*-
+from random import randint
+def potato(cenni, input):
+    cenni.write(['PRIVMSG', input.sender], '\x01ACTION is a potato\x01')
+potato.commands = ['potato']
+potato.priority = 'high'
+def moo(cenni, input):
+    cenni.say('MoooooOoooOooooooooooo')
+moo.commands = ['moo']
+moo.priority = 'high'
+def cookie(cenni, input):
+    nick = input.nick
+    if input.group(2):
+        nick = input.group(2)
+    cenni.write(['PRIVMSG', input.sender], '\x01ACTION gives ' + nick + ' a cookie\x01')
+cookie.commands = ['cookie']
+cookie.priority = 'high'
+def source(cenni, input):
+    cenni.say('Cenni: https://github.com/jztech101/cenni')
+source.commands = ['source']
+source.priority = 'high'
+def shrug(cenni, input):
+    cenni.say('┻━┻ ︵ ¯\_(ツ)_/¯ ︵ ┻━┻')
+shrug.commands = ['shrug']
+shrug.priority = 'high'
+def hmmm(cenni, input):
+#    cenni.say('t' + u'\u200b' + 'est')
+    cenni.say(cenni.hostmasks["JZTech101"])
+hmmm.commands = ['hmmm']
+hmmm.priority = 'high'
+if __name__ == '__main__':
+    print(__doc__.strip())
--- modules/topic.py
+++ modules/topic.py
@@ -10,6 +10,13 @@ def is_chan_admin(cenni, input, a):
             return True
     return False
 
+def getChanSplitChar(cenni, a):
+    if hasattr(cenni.config, 'topicsplit'):
+        if a in cenni.config.topicsplit:
+            return cenni.config.topicsplit[a]
+        else:
+            return
+
 def topic(cenni, input):
     """
     This gives admins the ability to change the topic.
@@ -20,15 +27,37 @@ def topic(cenni, input):
     argc = len(text)
     channel = input.sender
     topic = ' '.join(text[1:])
+    index = 1
     if tools.isChan(text[1], False):
         if argc < 2: return
         channel = text[1]
         topic = ' '.join(text[2:])
+        index = 2
     if not is_chan_admin(cenni,input,channel):
         return cenni.say('You must be an admin to perform this operation')
-    if topic == '':
-        return
-    cenni.write(['TOPIC', channel], topic)
+    if channel not in cenni.channeltopics:
+        currenttopic = None
+    else:
+        currenttopic = cenni.channeltopics[channel]
+    if currenttopic and getChanSplitChar(cenni, channel) and (len(text) > index+1 or text[index].startswith("-")):
+        char = getChanSplitChar(cenni, channel)
+        if text[index].startswith("-") and  "".join(text[index][1:]).isdigit() and len(currenttopic.split(char)) >= int("".join(text[index][1:])):
+           tmp = currenttopic.split(char)
+           tmp.pop(int("".join(text[index][1:]))-1)
+           currenttopic = char.join(tmp)
+        elif text[index].isdigit() and len(currenttopic.split(char)) >= int(text[index]):
+           tmp = currenttopic.split(char)
+           tmp[int(text[index])-1] = ' '.join(text[index+1:])
+           currenttopic = char.join(tmp)
+        elif text[index] == 'add':
+           tmp = currenttopic.split(char)
+           tmp.append(' '.join(text[index+1:]))
+           currenttopic = char.join(tmp)
+        else:
+           currenttopic = topic
+        cenni.write(['TOPIC', channel], currenttopic)
+    else:
+        cenni.write(['TOPIC', channel], topic)
     return
 topic.commands = ['topic']
 topic.priority = 'low'
