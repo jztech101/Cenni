a5615c23170fd500e5e1d74a74bc679c2e66cd00
Julian Zhou <jzhou2oo6@gmail.com>
2018-06-14 23:20:25 -0500

    Moved to Separate File

--- modules/adminchannel.py
+++ modules/adminchannel.py
@@ -321,28 +321,5 @@ def kickban (cenni, input):
 kickban.commands = ['kickban', 'kb', 'kban']
 kickban.priority = 'high'
 
-def topic(cenni, input):
-    """
-    This gives admins the ability to change the topic.
-    Note: One does *NOT* have to be an OP, one just has to be on the list of
-    admins.
-    """
-    text = input.group().split()
-    argc = len(text)
-    channel = input.sender
-    topic = ' '.join(text[1:])
-    if tools.isChan(text[1], False):
-        if argc < 2: return
-        channel = text[1]
-        topic = ' '.join(text[2:])
-    if not is_chan_admin(cenni,input,channel):
-        return cenni.say('You must be an admin to perform this operation')
-    if topic == '':
-        return
-    cenni.write(['TOPIC', channel], topic)
-    return
-topic.commands = ['topic']
-topic.priority = 'low'
-
 if __name__ == '__main__':
     print(__doc__.strip())
