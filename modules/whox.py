2e02e4de418adae0a35ea8b235491c57399a7b2a
Julian Zhou <jzhou2oo6@gmail.com>
2018-06-16 17:57:39 +0000

    Case Insensitive Matching

--- modules/whox.py
+++ modules/whox.py
@@ -14,29 +14,33 @@ host.commands = ['host', 'hostmask']
 host.example = "host nick"
 
 def account(cenni, input):
-    if input.group(2) in cenni.accounts and cenni.accounts[input.group(2)] != 0:
-       cenni.say(cenni.accounts[input.group(2)])
+    nick = input.group(2).lower()
+    if nick in cenni.accounts and cenni.accounts[nick] != 0:
+       cenni.say("Account: " + cenni.accounts[nick])
     else:
        cenni.say("No account found")
 account.commands = ['acct','account']
 account.example = 'account'
 def server(cenni, input):
-    if input.group(2) in cenni.servers:
-       cenni.say(cenni.servers[input.group(2)])
+    nick = input.group(2).lower()
+    if nick in cenni.servers:
+       cenni.say(input.group(2)+ " is connected to: " + cenni.servers[nick])
     else:
        cenni.say("No server found")
 server.commands = ['server']
 server.example = 'server'
 def realname(cenni, input):
-    if input.group(2) in cenni.realnames:
-       cenni.say(cenni.realnames[input.group(2)])
+    nick = input.group(2).lower()
+    if nick in cenni.realnames:
+       cenni.say("Real Name: " + cenni.realnames[nick])
     else:
        cenni.say("No real name found")
 realname.commands = ['realname']
 realname.example = 'realname'
 def checkip(cenni,input):
-    if input.group(2) in cenni.ipaddrs:
-       cenni.say(cenni.ipaddrs[input.group(2)])
+    nick = input.group(2).lower()
+    if nick in cenni.ipaddrs and cenni.ipaddrs[nick] != '255.255.255.255':
+       cenni.say("IP: " + cenni.ipaddrs[nick])
     else:
        cenni.say("No IP Found")
 checkip.commands = ['checkip','getip']
