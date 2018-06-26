ba78d10fd5253c720f6730ac0ffce1e56d7a7b3c
jztech101 <jzhou2oo6@gmail.com>
2018-06-26 15:13:31 +0000

    Fix cap

--- modules/help.py
+++ modules/help.py
@@ -9,7 +9,7 @@ def doc(cenni, input):
         else:
             cenni.say('No help found')
     else:
-        cenni.say('help: returns maybe helpful information')
+        cenni.say('help: Returns maybe helpful information')
 doc.commands = ['help']
 doc.example = 'help'
 doc.priority = 'low'
