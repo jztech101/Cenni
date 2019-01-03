9a1da2516e7ff6c4895c589ffa5175e57ee6b103
Julian Zhou <jzhou2oo6@gmail.com>
2019-01-03 23:39:45 +0000

    Fix variable name

--- modules/roulette.py
+++ modules/roulette.py
@@ -2,9 +2,9 @@
 import random
 import tools
 barrel = dict()
-current = dict()
+currentbarrel = dict()
 def roulette(cenni, input):
-    global barrel, current
+    global barrel, currentbarrel
     if not tools.isChan(input.sender, False):
         return
     if hasattr(cenni.config, 'rouletteC') and input.sender in cenni.config.rouletteC:
@@ -15,10 +15,10 @@ def roulette(cenni, input):
     random.seed()
     if input.sender not in barrel or barrel[input.sender] == None or (input.group(2) and input.group(2).lower() == 'spin'):
         barrel[input.sender] = random.randint(1,8)
-        current[input.sender] = 1
+        currentbarrel[input.sender] = 1
         if input.group(2) and input.group(2).lower() == 'spin':
             return cenni.say("Feeling lucky?")
-    if barrel[input.sender] == current[input.sender]:
+    if barrel[input.sender] == currentbarrel[input.sender]:
         if cenni.nick.lower() in cenni.ops[input.sender]:
             cenni.write(['KICK', input.sender, input.nick, "BANG"])
         else:
@@ -26,7 +26,7 @@ def roulette(cenni, input):
         barrel[input.sender] = None
     else:
         cenni.say("*CLICK*")
-        current[input.sender] += 1
+        currentbarrel[input.sender] += 1
 #        print(barrel[input.sender])
 roulette.commands = ['rr','roulette']
 roulette.priority= 'high'
